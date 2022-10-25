73b0e5cf431c8cd7238f1bee426bb24694316a35
Lucas Johnson <lwjohnson@mail.bradley.edu>
2022-10-25 18:43:13 -0500

    Tank prefab with basic movement controls

---
+++ CO2_Reducer_Simulator/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float turnSpeed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        float rotateInput = Input.GetAxisRaw("Horizontal") * turnSpeed * Time.deltaTime;
+        float moveInput = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.Translate(Vector3.forward * moveInput);
+        transform.Rotate(Vector3.up * rotateInput);
+    }
+}
