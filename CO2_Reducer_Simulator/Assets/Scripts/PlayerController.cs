b43fa9e8ed152632fc6aaa46a40615202298c69f
Lucas Johnson <lwjohnson@mail.bradley.edu>
2022-10-26 20:54:40 -0500

    Tank Shooting

--- CO2_Reducer_Simulator/Assets/Scripts/PlayerController.cs
+++ CO2_Reducer_Simulator/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject bulletPrefab;
+
     public float moveSpeed = 10f;
     public float turnSpeed = 10f;
+    public float shootForce = 10f;
+    public float timeBetweenShots = 0.5f;
+    private float nextShotTime;
+
 
     private bool tankRed = false;
     private Rigidbody rb;
+    private Transform firePoint;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        firePoint = transform.GetChild(1);
 
         if(gameObject.tag == "TankRed")
         {
@@ -23,15 +31,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        HandleMovement();
+        if (tankRed)
+        {
+            HandleMovement(true);
+            HandleShooting(KeyCode.E);
+        }
+        else
+        {
+            HandleMovement(false);
+            HandleShooting(KeyCode.Slash);
+        }
     }
 
-    void HandleMovement()
+    void HandleMovement(bool player1)
     {
         float rotateInput;
         float moveInput;
 
-        if (tankRed)
+        if (player1)
         {
             rotateInput = Input.GetAxisRaw("Horizontal") * turnSpeed * Time.deltaTime;
             moveInput = Input.GetAxisRaw("Vertical") * moveSpeed; //* Time.deltaTime;
@@ -44,5 +61,24 @@ public class PlayerController : MonoBehaviour
 
         rb.velocity = transform.forward * moveInput;
         transform.Rotate(Vector3.up * rotateInput);
+    }
+
+    void HandleShooting(KeyCode inputKey)
+    {
+        if(Input.GetKeyDown(inputKey))
+        {
+            // Limits the rate at which projectiles can be fired
+            bool canShoot = Time.time > nextShotTime;
+
+            if (canShoot)
+            {
+                GameObject spawnedBullet = Instantiate(bulletPrefab, firePoint.position, transform.rotation);
+                spawnedBullet.GetComponent<Rigidbody>().AddForce(transform.forward * shootForce, ForceMode.Impulse);
+                Destroy(spawnedBullet, 5f);
+
+                // Resets projectile delay timer
+                nextShotTime = Time.time + timeBetweenShots;
+            }
+        }
     }
 }
