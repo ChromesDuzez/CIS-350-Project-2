0bd05b0186aead43a2c3ab434d0b8e5450018983
Lucas Johnson <lwjohnson@mail.bradley.edu>
2022-10-26 19:00:44 -0500

    Better Tank Movement

--- CO2_Reducer_Simulator/Assets/Scripts/PlayerController.cs
+++ CO2_Reducer_Simulator/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,19 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 10f;
     public float turnSpeed = 10f;
 
+    private bool tankRed = false;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        if(gameObject.tag == "TankRed")
+        {
+            tankRed = true;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -15,10 +28,21 @@ public class PlayerController : MonoBehaviour
 
     void HandleMovement()
     {
-        float rotateInput = Input.GetAxisRaw("Horizontal") * turnSpeed * Time.deltaTime;
-        float moveInput = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+        float rotateInput;
+        float moveInput;
 
-        transform.Translate(Vector3.forward * moveInput);
+        if (tankRed)
+        {
+            rotateInput = Input.GetAxisRaw("Horizontal") * turnSpeed * Time.deltaTime;
+            moveInput = Input.GetAxisRaw("Vertical") * moveSpeed; //* Time.deltaTime;
+        }
+        else
+        {
+            rotateInput = Input.GetAxisRaw("Horizontal2") * turnSpeed * Time.deltaTime;
+            moveInput = Input.GetAxisRaw("Vertical2") * moveSpeed; //* Time.deltaTime;
+        }
+
+        rb.velocity = transform.forward * moveInput;
         transform.Rotate(Vector3.up * rotateInput);
     }
 }
