0f948c7d521b2397c93582fb2dbf31286a430379
cgamagami1 <81793604+cgamagami1@users.noreply.github.com>
2022-10-29 17:40:40 -0500

    Add Car AI
    -cars move from waypoint to waypoint

---
+++ CO2_Reducer_Simulator/Assets/Scripts/Car.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] Transform[] waypoints;
+    private NavMeshAgent agent;
+    private int currentWaypointIndex = 0;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        GoToNextWaypoint();
+    }
+
+    public void GoToNextWaypoint()
+    {
+        agent.SetDestination(waypoints[currentWaypointIndex].position);
+        currentWaypointIndex++;
+        if (currentWaypointIndex >= waypoints.Length)
+            currentWaypointIndex = 0;
+    }
+}
---
+++ CO2_Reducer_Simulator/Assets/Scripts/Waypoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Car"))
+        {
+            other.gameObject.GetComponent<Car>().GoToNextWaypoint();
+        }
+    }
+}
