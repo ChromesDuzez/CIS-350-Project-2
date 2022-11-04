44c2a9781854a2c75b01e90111e8925fd00db539
PlatFormPlayZ <zjrw@icloud.com>
2022-11-03 21:35:47 -0500

    Main Menu Created

---
+++ CO2_Reducer_Simulator/Assets/Scripts/GlobalSettings.cs
@@ -0,0 +1,23 @@
+﻿/*
+* Zach Wilson
+* CIS 350 - Group Project
+* This script stores global variables for the game such as volume, muted and senstivity
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalSettings : MonoBehaviour
+{
+    //Audio Settings
+    public static bool bUnMuted = true;
+    public static float volume = 1.0f;
+
+    //Control Settings
+    public static float mouseSense = 1.0f;
+
+    //Settings Related to Seeing the Tutorial on the start of the game...
+    public static bool hasSeenTutorial = false;
+    public static string tutorialScene;
+}
---
+++ CO2_Reducer_Simulator/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,68 @@
+﻿/*
+* Devun Schneider
+* CIS 350 - Trash Pick-Up Simulator
+* This script manages the changing of scenes throughout the menus
+*/
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "MainMenu")
+        {
+            Time.timeScale = 1.0f;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+    public void LoadScene(string sceneName)
+    {
+        Debug.Log("SceneLoader");
+        Debug.Log("sceneName to load: " + sceneName);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    //Zach Wilson -> so that the start button can send the player to the tutorial scene first if the player has not been yet
+    public void StartGame(string sceneName)
+    {
+        if(GlobalSettings.hasSeenTutorial)
+        {
+            Debug.Log("SceneLoader");
+            Debug.Log("sceneName to load: " + sceneName);
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            try
+            {
+                GlobalSettings.hasSeenTutorial = true; 
+                Debug.Log("SceneLoader");
+                Debug.Log("sceneName to load: " + GlobalSettings.tutorialScene);
+                if(SceneManager.GetSceneByName(GlobalSettings.tutorialScene).IsValid())
+                {
+                    SceneManager.LoadScene(GlobalSettings.tutorialScene);
+                }
+                else
+                {
+                    throw new UnityException("Tutorial Scene Not Found!");
+                }
+            }
+            catch
+            {
+                GlobalSettings.hasSeenTutorial = false;
+                Debug.Log("SceneLoader Failed to find Tutorial Scene: " + GlobalSettings.tutorialScene);
+                Debug.Log("SceneLoader");
+                Debug.Log("sceneName to load: " + sceneName);
+                SceneManager.LoadScene(sceneName);
+            }
+        }
+    }
+}
