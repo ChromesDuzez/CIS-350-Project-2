ed351b7480bfdd8ff320cb404ef2c94b8b978ebd
Devun Schneider <dschneider@mail.bradley.edu>
2022-12-04 19:10:25 -0600

    Added PauseMenu

---
+++ CO2_Reducer_Simulator/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void MainMenu()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(0);
+    }
+    public void PauseGame()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1.0f;
+        pauseMenu.SetActive(false);
+    }
+    public void QuitGame()
+    {
+        Time.timeScale = 1.0f;
+        Application.Quit();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        PauseGame();
+    }
+}
